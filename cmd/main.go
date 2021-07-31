a429af5bc68c18fe4c45e466cbe0df65be97a125
GolangLev <ctracer@inbox.ru@>
2021-07-31 15:11:33 +0300

    Добавлена библиотека viper для указания порта и создана функция IniConfig которая инициализирует конфигурации приложения

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,14 @@ import (
 	"github.com/GolangLev/Goland/pkg/handler"
 	"github.com/GolangLev/Goland/pkg/repository"
 	"github.com/GolangLev/Goland/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := InitConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	// Отображение зависимостей /*Репозиторий, сервисы общаются с репозиторием,
 	//а хэндлеры общаются с сервисами. Бизнес логика приложения*/
 	repos := repository.NewRepository()
@@ -16,7 +20,13 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running server: %s ", err.Error())
 	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
